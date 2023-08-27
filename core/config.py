71c6035f5cda11373bd6a4076b586f0e2efe43a2
Yair Knafo <knafoyair@gmail.com>
2023-08-27 21:06:27 +0300

    comment out openapi config

--- core/config.py
+++ core/config.py
@@ -7,11 +7,11 @@ from pydantic import BaseConfig
 class GlobalConfig(BaseConfig):
     title: str = "test"
     version: str = "1.0.0"
-    description: str = os.environ.get("DESCRIPTION")
-    openapi_prefix: str = os.environ.get("OPENAPI_PREFIX")
+    description: str = "bla"
+    # openapi_prefix: str = "api"
     docs_url: str = "/docs"
     redoc_url: str = "/redoc"
-    openapi_url: str = "/openapi.json"
+    # openapi_url: str = "/openapi.json"
     api_prefix: str = "/api"
 
 
--- main.py
+++ main.py
@@ -9,9 +9,9 @@ app = FastAPI(
     title=settings.title,
     version=settings.version,
     description=settings.description,
-    openapi_prefix=settings.openapi_prefix,
+    # openapi_prefix=settings.openapi_prefix,
     docs_url=settings.docs_url,
-    openapi_url=settings.openapi_url
+    # openapi_url=settings.openapi_url
 )
 
 app.include_router(router, prefix=settings.api_prefix)
