34615f88a400257009143c20a5a609c7fcf79046
Yair Knafo <knafoyair@gmail.com>
2023-08-27 12:59:19 +0300

    basic api

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, status
 
-from schemas.bands import BandRead
 
 router = APIRouter()
 
---
+++ core/config.py
@@ -0,0 +1,18 @@
+import os
+
+from pydantic import BaseConfig
+
+
+
+class GlobalConfig(BaseConfig):
+    title: str = "test"
+    version: str = "1.0.0"
+    description: str = os.environ.get("DESCRIPTION")
+    openapi_prefix: str = os.environ.get("OPENAPI_PREFIX")
+    docs_url: str = "/docs"
+    redoc_url: str = "/redoc"
+    openapi_url: str = "/openapi.json"
+    api_prefix: str = "/api"
+
+
+settings = GlobalConfig()
