7aac7433379bb71d20e589fe36579993578fc285
Yair Knafo <knafoyair@gmail.com>
2023-09-01 15:20:28 +0300

    add script populate_database.sql

---
+++ scripts/populate_database.sql
@@ -0,0 +1,4 @@
+INSERT INTO role (id, name, token) VALUES
+  ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Admin', 'admin_token123'),
+  ('a1eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'User', 'user_token456'),
+  ('a2eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'Guest', 'guest_token789');
