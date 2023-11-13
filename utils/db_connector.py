1f431b4cc886c260423111ad4d26d796f0fa03f4
Yair Knafo <knafoyair@gmail.com>
2023-11-13 22:42:51 +0200

    Intentionally introducing a potential hard coded password vulnerability for testing purposes

--- utils/db_connector.py
+++ utils/db_connector.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 db_username = os.getenv("POSTGRES_USER", "postgres")
-db_password = os.getenv("POSTGRES_PASSWORD", "postgres")
+# db_password = os.getenv("POSTGRES_PASSWORD", "postgres")
+# NOTE: Intentionally introducing a potential hard coded password vulnerability for testing purposes
+db_password = "postgres"
 db_host = os.getenv("DB_HOST", "localhost")  # usually "localhost" if it's running locally
 db_port = os.getenv("DB_PORT", "5432")  # default PostgreSQL port is 5432
 db_name = "token_db"
